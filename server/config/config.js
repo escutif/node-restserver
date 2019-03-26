da6def975fc649d714ced7fc71b08de8ab35c50e
escutif <escutif@gmail.com>
2019-03-26 15:52:41 -0300

    RestServer - usuario2

--- server/config/config.js
+++ server/config/config.js
@@ -13,15 +13,13 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ============================
 let urlDB;
 
-/*
+
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
     // urlDB = process.env.MONGO_URI;
-     urlDB = 'mongodb+srv://mongo-atlas:GttXIT8Tn7caZifH@cluster0-xsnum.mongodb.net/cafe';
+    urlDB = 'mongodb+srv://cafe-user:cafe-user24@cluster0-xsnum.mongodb.net/cafe';
 }
-*/
 
-urlDB = 'mongodb+srv://cafe-user:cafe-user24@cluster0-xsnum.mongodb.net/cafe';
 
 process.env.URLDB = urlDB;
