1ac4f255ce45cb2db17da51391481fec96459930
escutif <escutif@gmail.com>
2019-03-27 18:10:22 -0300

    Se agrega funcionalidad de login

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -2,11 +2,18 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const _ = require('underscore');
 const Usuario = require('../models/usuario');
+const { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion')
 const app = express();
 
 
 //get para obtener registros
-app.get('/usuario', function(req, res) {
+app.get('/usuario', verificaToken, function(req, res) {
+    // return res.json({
+    //     usuario: req.usuario,
+    //     nombre: req.usuario.nombre,
+    //     email: req.usuario.email
+    // })
+
     let desde = req.query.desde || 0;
     desde = Number(desde);
 
@@ -42,7 +49,7 @@ app.get('/usuario', function(req, res) {
 });
 
 //post para crear nuevos registros
-app.post('/usuario', function(req, res) {
+app.post('/usuario', [verificaToken, verificaAdmin_Role], function(req, res) {
     let body = req.body;
 
     let usuario = new Usuario({
@@ -53,14 +60,12 @@ app.post('/usuario', function(req, res) {
     });
 
     usuario.save((err, usuarioDB) => {
-
         if (err) {
             return res.status(400).json({
                 ok: false,
                 err
             });
         }
-
         // usuarioDB.password = null;
 
         res.json({
@@ -86,7 +91,7 @@ app.post('/usuario', function(req, res) {
 });
 
 //put para actualizar registros
-app.put('/usuario/:id', function(req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdmin_Role], function(req, res) {
     let id = req.params.id;
     //let body = req.body;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
@@ -112,7 +117,7 @@ app.put('/usuario/:id', function(req, res) {
 });
 
 //delete para eliminar registros o marcar como no disponible
-app.delete('/usuario/:id', function(req, res) {
+app.delete('/usuario/:id', [verificaToken, verificaAdmin_Role], function(req, res) {
 
     let id = req.params.id;
     /*   //Elimina de la BD
